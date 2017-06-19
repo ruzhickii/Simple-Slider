72cfb9f0195e3c14eb9f4aeff5f4ffb692c6c5f6
ruzhickii <ruzhickiisasha@gmail.com>
2017-06-19 19:36:06 +0300

    Simple Slider component with setInterval() !

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ var btn_next = document.querySelector('#gallery .buttons .next');
 var images = document.querySelectorAll('#gallery .photos img');
 var i = 0;
 
+window.onload = function () {
+  setInterval(func,3000)
+};
+
+function func () {
+    images[i].className = '';
+    i++;
+
+    if(i >= images.length){
+        i = 0;
+    }
+
+    images[i].className = 'showed';
+}
+
 btn_prev.onclick = function(){
     images[i].className = '';
     i--;
